27d7055ea25617a60726610b377663feee3d1ec8
e.serqueira <eduardo.serqueira@outlook.com>
2020-05-18 22:26:11 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercicio_doze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int horaInicial, horaFinal, duracao;
+
+            Console.WriteLine("Hora inicial: ");
+            horaInicial = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hora final: ");
+            horaFinal = int.Parse(Console.ReadLine());
+
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao = 24 - horaInicial + horaFinal;
+            }
+
+            Console.WriteLine("O JOGO DUROU {0} HORA(S)", duracao);
+            Console.ReadKey();
+        }
+    }
+}
